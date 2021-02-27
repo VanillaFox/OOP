01ad0f69ccb8548632ccc7225b39bec9c8a87bad
VanillaFox <71337244+VanillaFox@users.noreply.github.com>
2021-02-28 00:51:43 +0300

    Create factory.h

---
+++ lab_8/factory.h
@@ -0,0 +1,27 @@
+#pragma once
+#include<memory>
+#include"figures.h"
+
+template<typename T>
+class Factory {
+public:
+	static std::shared_ptr<Figure> CreateFigure(char* type) {
+		std::shared_ptr<Figure> fig;
+		std::cout << "Enter side: ";
+		T side;
+		if (type[0] == 's') {
+			std::cin >> side;
+			fig = std::make_shared<Square<T>>(side);
+		}
+		if (type[0] == 't') {
+			std::cin >> side;
+			fig = std::make_shared<Triangle<T>>(side);
+		}
+		if (type[0] == 'r') {
+			T height;
+			std::cin >> side >> height;
+			fig = std::make_shared<Rectangle<T>>(side, height);
+		}
+		return fig;
+	}
+};
