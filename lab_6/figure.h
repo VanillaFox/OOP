c7a0a1604de736a8ce9d9da651f64a20d8737017
VanillaFox <71337244+VanillaFox@users.noreply.github.com>
2021-02-28 00:47:17 +0300

    Create figure.h

---
+++ lab_6/figure.h
@@ -0,0 +1,17 @@
+#pragma once
+template<typename T>
+struct Figure {
+	using coords = std::pair<T, T>;
+	coords a, b, c, d;
+	Figure(T a) {
+		get_coords(*this, a);
+	}
+	Figure() {}
+	~Figure() {}
+};
+
+template<typename T, typename S>
+void get_coords(T& elem, S side) {
+	elem.a.first = elem.a.second = elem.d.first = elem.b.second = 0;
+	elem.b.first = elem.c.first = elem.c.second = elem.d.second = side;
+}
